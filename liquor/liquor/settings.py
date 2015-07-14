2e52bf010d6660fe4002b8d3f63136e8e1abb07c
AndrewKim <taeyeonkim90@gmail.com>
2015-07-14 00:14:35 -0700

    finished redirection of favorite page for unregistered user

--- liquor/liquor/settings.py
+++ liquor/liquor/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = (
     'debug_toolbar',
 )
 
+LOGIN_URL = '/register/'
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
